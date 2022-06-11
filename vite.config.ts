3cb8746028f41014e885b3acb9e29182a8686151
sinsky <60662802+sinsky@users.noreply.github.com>
2022-06-11 20:36:37 +0900

    change setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
     outDir: "./docs",
     rollupOptions: {
@@ -12,6 +13,7 @@ export default defineConfig({
         // entryFileNames: "[name].js",
       },
     },
+    emptyOutDir: false,
   },
   define: {
     global: "window",
