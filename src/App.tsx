0cb03b1c719cbc41f712fef80bc27476836c6817
sinsky <60662802+sinsky@users.noreply.github.com>
2022-06-11 23:38:08 +0900

    change design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,96 @@
-import { useState } from 'react'
+import { FaTwitterSquare, FaGithubSquare } from "react-icons/fa";
+import { useState } from "react";
+import MySkill from "./MySkill.json";
+
+type SkillProps = {
+  lang: string;
+  skills: string[];
+};
+type otherProps = {
+  title: string;
+  description: string;
+};
 
 function App() {
-
+  const [useSkill] = useState<SkillProps[]>(MySkill.languages);
+  const [others] = useState<otherProps[]>(MySkill.otherSkill);
   return (
-    <div className="App"></div>
-  )
+    <div className="bg-slate-300 text-center">
+      <div className="flex-flex-col snap-y snap-mandatory overflow-y-auto h-screen">
+        <div className="container m-auto h-screen flex flex-col items-center justify-around snap-center snap-always">
+          <div className="py-12">
+            <div>
+              <img
+                className="w-32 h-32 m-auto border rounded-full"
+                src="https://github.com/sinsky.png"
+                alt="My Icon"
+              />
+            </div>
+            <h1 className="font-bold text-4xl">sinsky</h1>
+          </div>
+          <div className="py-12">
+            <h2 className="font-bold text-3xl capitalize">About me</h2>
+            <div className="flex justify-around">
+              <div>
+                <a href="https://twitter.com/sin_sky_">
+                  <FaTwitterSquare size={40} className="hover:text-blue-500" />
+                </a>
+              </div>
+              <div>
+                <a href="https://github.com/sinsky">
+                  <FaGithubSquare size={40} className="hover:text-teal-700" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container m-auto h-screen flex flex-col items-center justify-around snap-center snap-always">
+          <div className="py-12">
+            <h2 className="font-bold text-3xl capitalize">Use skills</h2>
+            <div className="flex flex-col">
+              <div className="border-b border-teal-800 grid grid-cols-2 gap-4 font-bold py-4">
+                <div>Language</div>
+                <div>Framework/Library</div>
+              </div>
+              {useSkill.map((skillItem) => {
+                const { lang, skills } = skillItem;
+                return (
+                  <div className="border-b border-teal-800 grid grid-cols-2 gap-4 py-4">
+                    <div>{lang}</div>
+                    <div>
+                      {skills.map((skill) => (
+                        <div>{skill}</div>
+                      ))}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="container m-auto h-screen flex flex-col items-center justify-around snap-center snap-always">
+          <div className="py-12">
+            <h2 className="font-bold text-3xl capitalize">Other skills</h2>
+            <div className="flex flex-col">
+              <div className="border-b border-teal-800 grid grid-cols-2 gap-4 font-bold py-4">
+                <div></div>
+                <div>説明</div>
+              </div>
+              {others.map((other) => {
+                const { title, description } = other;
+                return (
+                  <div className="border-b border-teal-800 grid grid-cols-2 gap-4 py-4">
+                    <div>{title}</div>
+                    <div className="whitespace-normal">{description}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
