1fc658b0bbf2ad3d92ba1340b2b73f7fc5f41c97
sinsky <60662802+sinsky@users.noreply.github.com>
2022-06-12 05:00:55 +0900

    safari viewport support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        screen: ["100vh", "100dvh"],
+      },
+      minHeight: {
+        screen: ["100vh", "100dvh"],
+      },
+      maxHeight: {
+        screen: ["100vh", "100dvh"],
+      },
+    },
   },
   plugins: [],
 };
