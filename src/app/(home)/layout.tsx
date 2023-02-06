5bca36e69cda1eea8bf422ae77746a7f3743e8a8
sinsky <60662802+sinsky@users.noreply.github.com>
2023-02-06 21:17:50 +0900

    update layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -150,7 +150,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </div>
         {/* home */}
         <div className="flex flex-col flex-1 md:pl-64">
-          <div className="sticky top-0 z-10 pt-1 pl-1 bg-gray-100 sm:pl-3 sm:pt-3 md:hidden">
+          <div className="sticky top-0 z-10 pt-1 pl-1 bg-gray-200 border-b border-gray-400 sm:pl-3 sm:pt-3 md:hidden">
             <button
               type="button"
               className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
@@ -161,7 +161,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </button>
           </div>
           <main className="flex-1">
-            <div className="py-6">
+            <div className="py-2">
               <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
                 {children}
               </div>
