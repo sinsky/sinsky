c30d8aaf1ec766b2951d0990b66969fdf754c7e8
sinsky <60662802+sinsky@users.noreply.github.com>
2023-02-07 01:29:18 +0900

    create contact schema

---
+++ src/schema/contact.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const createContactSchema = z.object({
+  replyEmail: z.string().email("有効なメールアドレスを指定してください"),
+  subject: z
+    .string()
+    .min(1, "最低1文字以上の入力が必要です")
+    .max(100, "最大100文字までの入力となります"),
+  body: z
+    .string()
+    .min(5, "最低5文字以上の入力が必要です")
+    .max(500, "最大500文字までの入力となります"),
+});
+
+export type CreateContactSchema = z.TypeOf<typeof createContactSchema>;
