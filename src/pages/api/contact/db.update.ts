5a53587f0427afd9a8307cda560655df8a179bcb
sinsky <60662802+sinsky@users.noreply.github.com>
2023-02-11 21:34:45 +0900

    test db update function

--- src/pages/api/contact/db.update.ts
+++ src/pages/api/contact/db.update.ts
@@ -3,6 +3,7 @@ import { LineNotification } from "@/utils/notify.line";
 
 const handler = (req: NextApiRequest, res: NextApiResponse<undefined>) => {
   if (req.method === "POST") {
+    LineNotification(JSON.stringify(req.query));
   }
   res.status(200).send(undefined);
 };
