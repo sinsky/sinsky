bf3b651b9ede8e6f679f5fe35ea9a2f74f37aca8
sinsky <60662802+sinsky@users.noreply.github.com>
2023-03-01 00:10:09 +0900

    add metatag from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import type { Metadata } from "next";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,3 +8,45 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
+// https://beta.nextjs.org/docs/api-reference/metadata
+export const metadata: Metadata = {
+  title: {
+    default: "sinsky",
+    template: "%s - sinsky"
+  },
+  description: "こんにちは!sinskyです。PythonやTypeScriptを使用してWebサイトの開発を行うデベロッパーです。現在はGASを使って業務の自動化に取り組んでいます。柔軟な思考と問題解決能力に自信がありますので、課題解決に役立つプログラミング技術を提供できます。ご興味のある方はぜひお問い合わせください。",
+  authors: { name: "sinsky" },
+  creator: "sinsky",
+  keywords: ["React", "Next.js", "Python", "Django", "Developer", "デベロッパー", "サイト開発", "Webアプリケーション", "自動化", "GoogleAppsScript", "GAS"],
+  referrer: "origin-when-cross-origin",
+  openGraph: {
+    url: "https://sinsky.dev",
+    title: "Home - sinsky",
+    description: "こんにちは!sinskyです。PythonやTypeScriptを使用してWebサイトの開発を行うデベロッパーです。現在はGASを使って業務の自動化に取り組んでいます。柔軟な思考と問題解決能力に自信がありますので、課題解決に役立つプログラミング技術を提供できます。ご興味のある方はぜひお問い合わせください。",
+    siteName: "sinsky's portfolio",
+    locale: "ja_JP",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: false,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "standard",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/sinsky.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "sinsky's profile",
+    creator: "@sin_sky_",
+  },
+};
