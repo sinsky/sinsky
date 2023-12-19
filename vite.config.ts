9360c748f09d09f9f262a798dbfc2be62dd0d9fa
sinsky <60662802+sinsky@users.noreply.github.com>
2023-12-20 00:05:55 +0900

    chore: update outDir

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Sitemap from "vite-plugin-sitemap";
 
+const outDir = "./dist";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     Sitemap({
-      outDir: "./docs",
+      outDir,
       hostname: "https://sinsky.dev",
       generateRobotsTxt: true,
     }),
   ],
   base: "./",
   build: {
-    outDir: "./docs",
+    outDir,
     rollupOptions: {
       output: {
         // assetFileNames: "[name][extname]",
