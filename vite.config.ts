107d13f1437c72a235d1cb2aa721b701dfdc75e1
sinsky <60662802+sinsky@users.noreply.github.com>
2023-12-19 23:33:48 +0900

    feat: generate vite-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
+import Pages from "vite-plugin-pages";
+import generateSitemap from "vite-plugin-pages-sitemap";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Pages({ onRoutesGenerated: (routes) => generateSitemap({ routes }) }),
+  ],
   base: "./",
   build: {
     outDir: "./docs",
