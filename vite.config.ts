fe0af03b17550fef9b50d84043b59c8f9fbd72e8
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-27 02:04:30 +0900

    chore(vite.config.ts): organize import statements for better readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue(), VueRouter()],
 });
