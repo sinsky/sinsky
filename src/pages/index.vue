bf82fe3c0530444ab7e6d10770d6afc7c1d5fbc4
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-27 02:00:36 +0900

    fix(index.vue): update meta description and ogDescription to Japanese for localization purposes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,9 +4,12 @@ import { useSeoMeta } from "@unhead/vue";
 useSeoMeta({
   charset: "utf-8",
   title: "sinsky.me",
-  description: "Hi, I am sinsky. This is my self-introduction pages.",
+  description: "やっほー！sinskyのプロフィールページへようこそ！",
   ogImage: "https://github.com/sinsky.png",
-  ogDescription: "Hi, I am sinsky. This is my self-introduction pages.",
+  ogDescription: "やっほー！sinskyのプロフィールページへようこそ！",
+  ogSiteName: "sinsky.me",
+  ogType: "website",
+  ogUrl: "https://sinsky.me",
   ogLocale: "ja",
   themeColor: "#cbd5e1",
 });
