eedca0f2e57830a00ccd6c143781558ce5544b8d
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-27 02:09:30 +0900

    chore(main.ts): organize imports for better readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import { routes } from "vue-router/auto-routes";
+import "./assets/index.css";
 
 export const createApp = ViteSSG(
   App,
