4dc5a456c22ef4d572af83fe8c48c6bca0feb519
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-27 02:18:44 +0900

    feat(Top.vue): add avatar component with image and fallback for user display

---
+++ src/components/Top.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+</script>
+
+<template>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <Avatar size="lg">
+      <AvatarImage src="https://github.com/sinsky.png" alt="@sinsky" />
+      <AvatarFallback>SS</AvatarFallback>
+    </Avatar>
+    <h1 class="text-3xl font-bold">sinsky.me</h1>
+  </section>
+</template>
