d8f615b96238e023d6e9155b298fba8f47a4f7eb
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-27 02:07:08 +0900

    chore(vite.config.ts): add VueRouter plugin to enhance routing capabilities in the application

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
@@ -12,4 +13,9 @@ export default defineConfig({
     },
   },
   plugins: [vue(), VueRouter()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
