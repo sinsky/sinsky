01c5788dc8b1e228c9798989472e2e667f2a527f
sinsky <60662802+sinsky@users.noreply.github.com>
2024-12-27 00:40:16 +0900

    feat(index.html): add favicon and theme color meta tags for better branding and user experience feat(index.vue): implement SEO metadata for improved search engine visibility and social sharing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { useSeoMeta } from "@unhead/vue";
+
+useSeoMeta({
+  charset: "utf-8",
+  title: "sinsky.me",
+  description: "Hi, I am sinsky. This is my self-introduction pages.",
+  ogImage: "https://github.com/sinsky.png",
+  ogDescription: "Hi, I am sinsky. This is my self-introduction pages.",
+  ogLocale: "ja",
+  themeColor: "#cbd5e1",
+});
+</script>
+
 <template>
   <h1>I am sinsky.</h1>
 </template>
