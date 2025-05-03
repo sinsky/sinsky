4d6e8f64508eb66a4f8c31d4ce43cc4e9506374c
sinsky <60662802+sinsky@users.noreply.github.com>
2025-05-04 01:38:05 +0900

    feat(vite.config.ts): add PostCSS configuration with Tailwind CSS and Autoprefixer plugins to enhance CSS processing

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
-import autoprefixer from "autoprefixer";
-import tailwind from "tailwindcss";
 import vueDevTools from "vite-plugin-vue-devtools";
 import Icons from "unplugin-icons/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
   plugins: [vue(), VueRouter(), vueDevTools(), Icons()],
   resolve: {
     alias: {
