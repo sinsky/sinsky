dd424a37ef32926519f8992d3693120e9f18df98
sinsky <60662802+sinsky@users.noreply.github.com>
2025-05-04 03:00:12 +0900

    chore: update Avatar component from shadcn-vue

--- src/components/ui/avatar/Avatar.vue
+++ src/components/ui/avatar/Avatar.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from "vue";
 import { cn } from "@/lib/utils";
-import { AvatarRoot } from "radix-vue";
-import { avatarVariant, type AvatarVariants } from ".";
+import { AvatarRoot } from "reka-ui";
 
-const props = withDefaults(
-  defineProps<{
-    class?: HTMLAttributes["class"];
-    size?: AvatarVariants["size"];
-    shape?: AvatarVariants["shape"];
-  }>(),
-  {
-    size: "sm",
-    shape: "circle",
-  },
-);
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
-  <AvatarRoot :class="cn(avatarVariant({ size, shape }), props.class)">
+  <AvatarRoot
+    data-slot="avatar"
+    :class="
+      cn(
+        'relative flex size-8 shrink-0 overflow-hidden rounded-full',
+        props.class,
+      )
+    "
+  >
     <slot />
   </AvatarRoot>
 </template>
