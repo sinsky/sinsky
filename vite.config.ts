d9e6dbcc270303c31e5c81b10d1b966f9aff24d0
sinsky <60662802+sinsky@users.noreply.github.com>
2025-05-04 01:38:29 +0900

    feat(vite.config.ts): add Tailwind CSS plugin for Vite to enable Tailwind CSS integration

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueRouter(), vueDevTools(), Icons()],
+  plugins: [vue(), VueRouter(), vueDevTools(), Icons(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
